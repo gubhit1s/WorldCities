632eb56f5cd0858ab15ca6187757390916f430df
Tran Minh <thaiminh8xpro@hotmail.com>
2022-08-10 08:14:56 +0700

    Adding debouncing

--- WorldCities/src/app/cities/cities.component.ts
+++ WorldCities/src/app/cities/cities.component.ts
@@ -4,6 +4,9 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 import { environment } from './../../environments/environment';
 import { City } from './city';
 
@@ -27,11 +30,22 @@ export class CitiesComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
+  filterTextChanged: Subject<string> = new Subject<string>();
+
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
     this.loadData();
+  }
+
+  onFilterTextChanged(filterText: string) {
+    if (this.filterTextChanged.observers.length === 0) {
+      this.filterTextChanged.pipe(debounceTime(1000), distinctUntilChanged()).subscribe(query => {
+        this.loadData(query);
+      });
+    }
+    this.filterTextChanged.next(filterText);
   }
 
   loadData(query?: string) {
