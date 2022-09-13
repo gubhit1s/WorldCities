c74bff74528e47bad7355f369398dc8a9e6a9ab9
Tran Minh <thaiminh8xpro@hotmail.com>
2022-09-14 01:08:05 +0700

    Create Default Test Users

---
+++ WorldCitiesAPI.Tests/SeedController_Tests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System.Threading.Tasks;
+using WorldCitiesAPI.Controllers;
+using WorldCitiesAPI.Data;
+using WorldCitiesAPI.Data.Models;
+using Xunit;
+
+namespace WorldCitiesAPI.Tests;
+
+public class SeedController_Tests
+{
+    /// <summary>
+    /// Test the CreateDefaultUsers() method.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task CreateDefaultUsers()
+    {
+        // Arrange
+        // create option instances required by the ApplicationDbContext
+        DbContextOptions<ApplicationDbContext> options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                        .UseInMemoryDatabase(databaseName: "WorldCities").Options;
+        IWebHostEnvironment mockEnv = Mock.Of<IWebHostEnvironment>();
+        Mock<IConfiguration> mockConfiguration = new();
+    }
+}
--- WorldCitiesAPI/Controllers/SeedController.cs
+++ WorldCitiesAPI/Controllers/SeedController.cs
@@ -4,150 +4,165 @@ using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
 using WorldCitiesAPI.Data;
 using WorldCitiesAPI.Data.Models;
+using Microsoft.AspNetCore.Identity;
 
-namespace WorldCitiesAPI.Controllers
+namespace WorldCitiesAPI.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class SeedController : ControllerBase
 {
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class SeedController : ControllerBase
+    private readonly ApplicationDbContext _context;
+    private readonly IWebHostEnvironment _env;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IConfiguration _configuration;
+
+    public SeedController(
+        ApplicationDbContext context,
+        IWebHostEnvironment env,
+        RoleManager<IdentityRole> roleManager,
+        UserManager<ApplicationUser> userManager,
+        IConfiguration configuration)
     {
-        private readonly ApplicationDbContext _context;
-        private readonly IWebHostEnvironment _env;
+        _context = context;
+        _env = env;
+        _roleManager = roleManager;
+        _userManager = userManager;
+        _configuration = configuration;
+    }
 
-        public SeedController(
-            ApplicationDbContext context,
-            IWebHostEnvironment env)
+    [HttpGet]
+    public async Task<ActionResult> Import()
+    {
+        // prevents non-development environments from running this method
+        if (!_env.IsDevelopment())
+            throw new SecurityException("Not allowed");
+
+        var path = System.IO.Path.Combine(
+            _env.ContentRootPath,
+            "Data/Source/worldcities.xlsx");
+
+        using var stream = System.IO.File.OpenRead(path);
+        using var excelPackage = new ExcelPackage(stream);
+
+        // get the first worksheet 
+        var worksheet = excelPackage.Workbook.Worksheets[0];
+
+        // define how many rows we want to process 
+        var nEndRow = worksheet.Dimension.End.Row;
+
+        // initialize the record counters 
+        var numberOfCountriesAdded = 0;
+        var numberOfCitiesAdded = 0;
+
+        // create a lookup dictionary 
+        // containing all the countries already existing 
+        // into the Database (it will be empty on first run).
+        var countriesByName = _context.Countries
+            .AsNoTracking()
+            .ToDictionary(x => x.Name, StringComparer.OrdinalIgnoreCase);
+
+        // iterates through all rows, skipping the first one 
+        for (int nRow = 2; nRow <= nEndRow; nRow++)
         {
-            _context = context;
-            _env = env;
+            var row = worksheet.Cells[
+                nRow, 1, nRow, worksheet.Dimension.End.Column];
+
+            var countryName = row[nRow, 5].GetValue<string>();
+            var iso2 = row[nRow, 6].GetValue<string>();
+            var iso3 = row[nRow, 7].GetValue<string>();
+
+            // skip this country if it already exists in the database
+            if (countriesByName.ContainsKey(countryName))
+                continue;
+
+            // create the Country entity and fill it with xlsx data 
+            var country = new Country
+            {
+                Name = countryName,
+                ISO2 = iso2,
+                ISO3 = iso3
+            };
+
+            // add the new country to the DB context 
+            await _context.Countries.AddAsync(country);
+
+            // store the country in our lookup to retrieve its Id later on
+            countriesByName.Add(countryName, country);
+
+            // increment the counter 
+            numberOfCountriesAdded++;
         }
 
-        [HttpGet]
-        public async Task<ActionResult> Import()
+        // save all the countries into the Database 
+        if (numberOfCountriesAdded > 0)
+            await _context.SaveChangesAsync();
+
+        // create a lookup dictionary
+        // containing all the cities already existing 
+        // into the Database (it will be empty on first run). 
+        var cities = _context.Cities
+            .AsNoTracking()
+            .ToDictionary(x => (
+                Name: x.Name,
+                Lat: x.Lat,
+                Lon: x.Lon,
+                CountryId: x.CountryId));
+
+        // iterates through all rows, skipping the first one 
+        for (int nRow = 2; nRow <= nEndRow; nRow++)
         {
-            // prevents non-development environments from running this method
-            if (!_env.IsDevelopment())
-                throw new SecurityException("Not allowed");
+            var row = worksheet.Cells[
+                nRow, 1, nRow, worksheet.Dimension.End.Column];
 
-            var path = System.IO.Path.Combine(
-                _env.ContentRootPath,
-                "Data/Source/worldcities.xlsx");
+            var name = row[nRow, 1].GetValue<string>();
+            var nameAscii = row[nRow, 2].GetValue<string>();
+            var lat = row[nRow, 3].GetValue<decimal>();
+            var lon = row[nRow, 4].GetValue<decimal>();
+            var countryName = row[nRow, 5].GetValue<string>();
 
-            using var stream = System.IO.File.OpenRead(path);
-            using var excelPackage = new ExcelPackage(stream);
+            // retrieve country Id by countryName
+            var countryId = countriesByName[countryName].Id;
 
-            // get the first worksheet 
-            var worksheet = excelPackage.Workbook.Worksheets[0];
+            // skip this city if it already exists in the database
+            if (cities.ContainsKey((
+                Name: name,
+                Lat: lat,
+                Lon: lon,
+                CountryId: countryId)))
+                continue;
 
-            // define how many rows we want to process 
-            var nEndRow = worksheet.Dimension.End.Row;
-
-            // initialize the record counters 
-            var numberOfCountriesAdded = 0;
-            var numberOfCitiesAdded = 0;
-
-            // create a lookup dictionary 
-            // containing all the countries already existing 
-            // into the Database (it will be empty on first run).
-            var countriesByName = _context.Countries
-                .AsNoTracking()
-                .ToDictionary(x => x.Name, StringComparer.OrdinalIgnoreCase);
-
-            // iterates through all rows, skipping the first one 
-            for (int nRow = 2; nRow <= nEndRow; nRow++)
+            // create the City entity and fill it with xlsx data 
+            var city = new City
             {
-                var row = worksheet.Cells[
-                    nRow, 1, nRow, worksheet.Dimension.End.Column];
+                Name = name,
+                Lat = lat,
+                Lon = lon,
+                CountryId = countryId
+            };
 
-                var countryName = row[nRow, 5].GetValue<string>();
-                var iso2 = row[nRow, 6].GetValue<string>();
-                var iso3 = row[nRow, 7].GetValue<string>();
+            // add the new city to the DB context 
+            _context.Cities.Add(city);
 
-                // skip this country if it already exists in the database
-                if (countriesByName.ContainsKey(countryName))
-                    continue;
-
-                // create the Country entity and fill it with xlsx data 
-                var country = new Country
-                {
-                    Name = countryName,
-                    ISO2 = iso2,
-                    ISO3 = iso3
-                };
-
-                // add the new country to the DB context 
-                await _context.Countries.AddAsync(country);
-
-                // store the country in our lookup to retrieve its Id later on
-                countriesByName.Add(countryName, country);
-
-                // increment the counter 
-                numberOfCountriesAdded++;
-            }
-
-            // save all the countries into the Database 
-            if (numberOfCountriesAdded > 0)
-                await _context.SaveChangesAsync();
-
-            // create a lookup dictionary
-            // containing all the cities already existing 
-            // into the Database (it will be empty on first run). 
-            var cities = _context.Cities
-                .AsNoTracking()
-                .ToDictionary(x => (
-                    Name: x.Name,
-                    Lat: x.Lat,
-                    Lon: x.Lon,
-                    CountryId: x.CountryId));
-
-            // iterates through all rows, skipping the first one 
-            for (int nRow = 2; nRow <= nEndRow; nRow++)
-            {
-                var row = worksheet.Cells[
-                    nRow, 1, nRow, worksheet.Dimension.End.Column];
-
-                var name = row[nRow, 1].GetValue<string>();
-                var nameAscii = row[nRow, 2].GetValue<string>();
-                var lat = row[nRow, 3].GetValue<decimal>();
-                var lon = row[nRow, 4].GetValue<decimal>();
-                var countryName = row[nRow, 5].GetValue<string>();
-
-                // retrieve country Id by countryName
-                var countryId = countriesByName[countryName].Id;
-
-                // skip this city if it already exists in the database
-                if (cities.ContainsKey((
-                    Name: name,
-                    Lat: lat,
-                    Lon: lon,
-                    CountryId: countryId)))
-                    continue;
-
-                // create the City entity and fill it with xlsx data 
-                var city = new City
-                {
-                    Name = name,
-                    Lat = lat,
-                    Lon = lon,
-                    CountryId = countryId
-                };
-
-                // add the new city to the DB context 
-                _context.Cities.Add(city);
-
-                // increment the counter 
-                numberOfCitiesAdded++;
-            }
-
-            // save all the cities into the Database 
-            if (numberOfCitiesAdded > 0)
-                await _context.SaveChangesAsync();
-
-            return new JsonResult(new
-            {
-                Cities = numberOfCitiesAdded,
-                Countries = numberOfCountriesAdded
-            });
+            // increment the counter 
+            numberOfCitiesAdded++;
         }
+
+        // save all the cities into the Database 
+        if (numberOfCitiesAdded > 0)
+            await _context.SaveChangesAsync();
+
+        return new JsonResult(new
+        {
+            Cities = numberOfCitiesAdded,
+            Countries = numberOfCountriesAdded
+        });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> CreateDefaultUsers()
+    {
+        throw new NotImplementedException();
     }
 }
