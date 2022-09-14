99b9ef480fef6a116d94aa236630f4eac4f6b094
gubhit1s <101991304+gubhit1s@users.noreply.github.com>
2022-09-14 13:51:31 +0700

    Update SeedController_Tests.cs

--- WorldCitiesAPI.Tests/SeedController_Tests.cs
+++ WorldCitiesAPI.Tests/SeedController_Tests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -8,24 +8,54 @@ using System.Threading.Tasks;
 using WorldCitiesAPI.Controllers;
 using WorldCitiesAPI.Data;
 using WorldCitiesAPI.Data.Models;
-using Xunit;
+using XUnit;
 
 namespace WorldCitiesAPI.Tests;
-
-public class SeedController_Tests
-{
+public class SeedController_Tests {
+    
     /// <summary>
-    /// Test the CreateDefaultUsers() method.
+    /// Test the CreateDefaultUsers() method
     /// </summary>
-    /// <returns></returns>
     [Fact]
-    public async Task CreateDefaultUsers()
-    {
+    public async Task CreateDefaultUsers() {
         // Arrange
-        // create option instances required by the ApplicationDbContext
-        DbContextOptions<ApplicationDbContext> options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                        .UseInMemoryDatabase(databaseName: "WorldCities").Options;
-        IWebHostEnvironment mockEnv = Mock.Of<IWebHostEnvironment>();
-        Mock<IConfiguration> mockConfiguration = new();
-    }
+        // Create the option instances required by the ApplicationDbContext
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "WorldCiies").Options;
+            
+        // Create a IWebHostEnvironment mock instance
+        var mockEnv = Mock.Of<IWebHostEnvironment>();
+        
+        // Create an IConfiguration mock instance
+        var mockConfiguration = new Mock<IConfiguration>();
+        mockConfiguration.SetupGet(x => x[It.Is<string>(s => s == "DefaultPasswords:RegisteredUser")]).Returns("M0ckP$$word");
+		mockConfiguration.SetupGet(x => x[It.Is<string>(s => s == "DefaultPasswords:Administrator")]).Returns("M0ckP$$word");
+        
+        // Create a ApplicationDbContext instance using the in-memory Db
+        using var context = new ApplicationDbContext(options);
+		
+		// Create a RoleManager Instance
+		var roleManager = IdentityHelper.GetRoleManager(new RoleStore<IdentityRole>(context));
+		var userManager = IdentityHelper.GetUserManager(new UserStore<ApplicationUser>(context));
+		
+		// Create a SeedController instance
+		var controller = new SeedController(context, roleManager, userManager, mockEnv, mockConfiguration.Object);
+		
+		// Define the variables for the users we want to test
+		ApplicationUser user_Admin = null!;
+		ApplicationUser user_User = null!;
+		ApplicationUser user_NotExisting = null!;
+		
+		// Act
+		// Execute the SeedController's CreateDefaultUsers() method to create default users (and roles)
+		await controller.CreateDefaultUsers();
+		user_Admin = await userManager.FindByEmailAsync("admin@email.com");
+		user_User = await userManager.FindByEmailAsync("user@email.com");
+		user_NotExisting = await userManager.FindByEmailAsync("notexisting@email.com");
+		
+		// Assert
+		Assert.NotNull(user_Admin);
+		Assert.NotNull(user_User);
+		Assert.NotNull(user_NotExisting);
+	}
 }
