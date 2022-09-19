551e367436920d30a194f0a8ec8233fe17795242
gubhit1s <101991304+gubhit1s@users.noreply.github.com>
2022-09-19 17:56:34 +0700

    Create Query.cs

---
+++ WorldCitiesAPI/Data/GraphQL/Query.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using WorldCitiesAPI.Data.Models;
+
+namespace WorldCitiesAPI.Data.GraphQL;
+
+public class Query {
+
+    /// <summary>
+    /// Gets all Cities.
+    /// </summary>
+    [Serial]
+    [UsePaging]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<City> GetCities([Service] ApplicationDbContext context) => context.Cities;
+    
+    /// <summary>
+    /// Gets all countries.
+    /// </summary>
+    [Serial]
+    [UsePaging]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<Country> GetCountries([Service] ApplicationDbContext context) => context.Countries;
+}
